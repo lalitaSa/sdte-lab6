141a98a882bd2077798c8236ce5f410f1c17c9e4
lalitaSa <42977268+lalitaSa@users.noreply.github.com>
2021-02-22 16:54:11 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hi", (req, res) => {
-  res.send("Hello world!");
+  res.send("lalala 61070346");
 });
 
 module.exports = app;
